d0581a7848237ca17be737480655ab38faffee23
gecomerce <ti@gecomerce.com.br>
2026-08-25 16:59:14 -0300

    update

--- assets/js/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const btnToggle = document.querySelector('.navbar-toggler');
-    const menu = document.getElementById('navbarSupportedContent');
-
-    btnToggle.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
